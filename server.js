9a87bfb659402e537acb4ca96847f5746b6cf134
terenceDev <terence.arseneault@hotmail.com>
2023-08-22 16:47:37 -0300

    added main handlebars view in main.handlebars

--- server.js
+++ server.js
@@ -10,9 +10,17 @@ const helpers = require("./utils/helpers");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 const hbs = exphbs.create({ helpers });
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("./layouts/main");
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
