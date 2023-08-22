4566a8025d229ae9a28a8fa3e9a1d7d6e77a1482
terenceDev <terence.arseneault@hotmail.com>
2023-08-22 07:43:57 -0300

    connected to database successfully

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
