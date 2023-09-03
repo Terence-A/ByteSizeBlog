f55d701ca4930a31f7aecb06621af2dc40e85573
terenceDev <terence.arseneault@hotmail.com>
2023-09-03 09:19:23 -0300

    added the Comment model and set up index in models

--- models/index.js
+++ models/index.js
@@ -1,8 +1,39 @@
 const User = require("./User");
 const Post = require("./post");
-console.log("am i exporting");
-Post.belongsTo(User, {
-  foreignKey: "userId",
-  onDelete: "CASCADE",
+const Comment = require("./Comment");
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
 });
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+  hooks: true,
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
+  hooks: true,
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+  hooks: true,
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
+  hooks: true,
+});
+
+// Export the modules
+module.exports = { User, Post, Comment };
+
 module.exports = { User, Post };
